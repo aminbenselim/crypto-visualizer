188266ea63a7108b0242ecff2659b743b87bc65d
Amine Benselim <amine.benselim@friday.de>
2018-03-18 21:44:10 +0100

    fix webpack config for scss for both development and production

--- app/config/webpack.config.dev.js
+++ app/config/webpack.config.dev.js
@@ -163,6 +163,7 @@ module.exports = {
               {
                 loader: require.resolve('css-loader'),
                 options: {
+                  modules: true,
                   importLoaders: 1,
                   localIdentName: '[name]__[local]--[hash:base64:5]'
                 },
